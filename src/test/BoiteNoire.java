af9a74816546d6d7c154d12b02803f72d482ecfb
Marc-André Brochu <brochu.marca@gmail.com>
2020-11-29 19:51:21 -0500

    Couverture BoiteNoire - Currency.convert

--- src/test/BoiteNoire.java
+++ src/test/BoiteNoire.java
@@ -6,17 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static currencyConverter.MainWindow.convert;
+import static currencyConverter.Currency.convert;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class BoiteNoire {
     // Partition des entrees et valeurs frontieres
     double normalInput = 20;
     double nullInput = 0;
-    double extremeUpperInput = Integer.MAX_VALUE;
-    double extremeLowerInput = Integer.MIN_VALUE;
+    double extremeUpperInput = Double.MAX_VALUE;
+    double extremeLowerInput = Double.MIN_VALUE;
 
     //values needed for the test
-    private ArrayList<Currency> currencies = Currency.init();
+    private final ArrayList<Currency> currencies = Currency.init();
 
    //conversion name
     //"US Dollar", "Euro", "British Pound", "Swiss Franc", "Chinese Yuan Renminbi", "Japanese Yen"
@@ -51,5 +53,22 @@ public class BoiteNoire {
     @Test
     public void testExtremeLowerInput() {
         assertEquals(usdToEuroToPoundToYuan(extremeLowerInput), usdToFrancToYenToYuan(extremeLowerInput));
+    }
+
+
+    //currencyConverter.Currency.convert(Double, Double)
+    @Test
+    public void test_CurrencyConvert_normal() {
+        assertEquals(convert(normalInput, normalInput), normalInput*normalInput);
+    }
+
+    @Test
+    public void test_CurrencyConvert_extreme() {
+        assertNotEquals(convert(extremeUpperInput, extremeUpperInput), extremeUpperInput*extremeUpperInput);
+    }
+
+    @Test
+    public void test_CurrencyConvert_null() {
+        assertEquals(convert(nullInput, nullInput), 0);
     }
 }
